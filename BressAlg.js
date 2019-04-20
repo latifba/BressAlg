e5c8856ce1add667d13632842872b5df6cec6015
Latif Ba <lba.askia@gmail.com>
2019-04-20 17:34:23 -0400

    V3

--- BressAlg.js
+++ BressAlg.js
@@ -1,4 +1,4 @@
-
+// square obj
 function Square(size, x, y) {
     this.size = size
     this.x = x
@@ -6,22 +6,25 @@ function Square(size, x, y) {
     this.fill = "#000"
 }
 
+// grid obj
 function Grid(size, canvas) {
-    this.size = size
+    this.size = size // size of all squares (uniform)
     this.canvas = canvas
-    this.arr = []
+    this.arr = [] // 2D array of squares
     this.dim = canvas.width / this.size
-    this.selected = 0
-    this.bressMode = true
-    this.drawingFirstPixel = false
-    this.squareA = null
-    this.squareB = null
+    this.selected = 0 // squares selected
+    this.bressMode = true // true: draw line at once, false: step
+    this.drawingFirstPixel = false // 1st pixel in step mode
+    this.squareA = null // 1st square selected
+    this.squareB = null // 2nd square selected
 
+    // algorithm variables (to capture in step mode)
     this.x0 = null, this.y0 = null
     this.x1 = null, this.y1 = null
     this.x = null, this.y = null, this.d = null
     this.xi = null, this.xi = null
 
+    // create all the squares of the grid (size^2 squares)
     for (var i = 0; i < size; i++) {
         this.arr.push([])
         for (var j = 0; j < size; j++) {
@@ -29,11 +32,16 @@ function Grid(size, canvas) {
         }
     }
 
+    // draw empty grid
     this.draw = function() {
+        // squares have black outline and white fill
         canvas.ctx.strokeStyle = "#000"
         canvas.ctx.fillStyle = "#fff"
+
+        // clear canvas
         canvas.ctx.fillRect(0, 0, canvas.width, canvas.height)
 
+        // draw each square
         for (var i = 0; i < this.arr.length; i++) {
             for (var j = 0; j < this.arr[i].length; j++) {
                 let square = this.arr[i][j]
@@ -42,6 +50,7 @@ function Grid(size, canvas) {
         }
     }
 
+    // deselect squares and reset algorithm variables
     this.reset = function() {
         this.draw()
         this.squareA = null
@@ -53,6 +62,7 @@ function Grid(size, canvas) {
         this.bressMode = true
     }
 
+    // initialize the start & end point
     this.init = function() {
         let coordA = this.getCoord(this.squareA)
         let coordB = this.getCoord(this.squareB)
@@ -62,46 +72,55 @@ function Grid(size, canvas) {
         this.xi = 1, this.yi = 1
     }
 
+    // select square
     this.drawSquare = function(square) {
-        canvas.ctx.fillStyle = square.fill
-    //    if (this.bressMode && this.selected < 2) {
-            if (!this.squareA)
-                this.squareA = square
-            else if (!this.squareB)
-                this.squareB = square
+        canvas.ctx.fillStyle = square.fill // color
 
-            canvas.ctx.fillRect(square.x, square.y, square.size, square.size)
-            this.selected++
-    //    }
+        // record 1st or 2nd square
+        if (!this.squareA)
+            this.squareA = square
+        else if (!this.squareB)
+            this.squareB = square
+
+        canvas.ctx.fillRect(square.x, square.y, square.size, square.size) // draw
+        this.selected++
     }
 
+    // get square from local coordinates
     this.getSquare = function(x, y) {
         let i = parseInt(x / this.dim)
         let j = parseInt(y / this.dim)
         return this.arr[i][j]
     }
 
+    // get standard plane coordinates from a square
     this.getCoord = function(square) {
         return [parseInt(square.x / square.size),
-                parseInt((canvas.height - square.size - square.y) / square.size)]
+                parseInt((canvas.height - square.size - square.y) / square.size)] // reflection over x-axis
     }
 
+    // get square from standard plane coordinates
     this.getSquareFromCoord = function(x, y) {
         return this.arr[x][size - 1 - y]
     }
 
+    // draw green guideline between two selected squares
     this.drawGuideline = function() {
         canvas.ctx.strokeStyle = "#00ff00"
         let off = this.squareA.size/2
+        // draw from the middle of the 1st square to the middle of the 2nd square
         canvas.ctx.beginPath()
         canvas.ctx.moveTo(this.squareA.x + off, this.squareA.y + off)
         canvas.ctx.lineTo(this.squareB.x + off, this.squareB.y + off)
         canvas.ctx.stroke()
     }
 
+    // drwc 2 red dots on each square drawn
     this.drawDots = function(square) {
         let off = this.squareA.size
         this.canvas.ctx.fillStyle = "#ff0000"
+        // first circle is on the right side of the square in the middle
+        // second circle is between the first & top right corner
         this.canvas.ctx.beginPath()
         this.canvas.ctx.arc(square.x + off, square.y, off/4, 0, 2 * Math.PI)
         this.canvas.ctx.arc(square.x + off, square.y + off/2, off/4, 0, 2 * Math.PI)
@@ -109,31 +128,35 @@ function Grid(size, canvas) {
     }
 
 
+    // draw line between two squares using the empty squares between them
     this.drawLine = function(bressMode) {
         this.init()
+        // if user selected step mode but wasn't already in step mode
         this.drawingFirstPixel = this.bressMode == true && bressMode == false
         this.bressMode = bressMode
 
+        // from init
         let x0 = this.x0, y0 = this.y0,
             x1 = this.x1, y1 = this.y1
 
-        if (Math.abs(y1 - y0) < Math.abs(x1 - x0)) {
-            if (x0 > x1)
-                this.drawLineLow(x1, y1, x0, y0)
+        if (Math.abs(y1 - y0) < Math.abs(x1 - x0)) { // if dy < dx, then 1 > slope > -1
+            if (x0 > x1) // which point was placed first
+                this.drawLineLow(x1, y1, x0, y0) // placed backwards
             else
-                this.drawLineLow(x0, y0, x1, y1)
+                this.drawLineLow(x0, y0, x1, y1) // placed fowards
         }
-        else {
+        else { // if dy > dx, then 1 < slope < -1
             if (y0 > y1)
                 this.drawLineHigh(x1, y1, x0, y0)
             else
                 this.drawLineHigh(x0, y0, x1, y1)
         }
 
-        if (!this.bressMode)
+        if (!this.bressMode) // if step mode
             this.drawGuideline()
     }
 
+    // draw a line with a flatter slope
     this.drawLineLow = function(x0, y0, x1, y1) {
         let dx = x1 - x0
         let dy = y1 - y0
@@ -141,42 +164,43 @@ function Grid(size, canvas) {
         dxEl.innerHTML = dx
         dyEl.innerHTML = dy
 
-        if (dy < 0) {
+        if (dy < 0) { // if slope < 0
             this.yi *= -1
             dy *= -1
         }
 
-        let d = 2 * dy - dx
-        let y = y0
+        let d = 2 * dy - dx // see bress algorithm documentation
+        let y = y0 // y may increment or stay the same as x increments
 
-        if (!this.bressMode) {
-            if (this.drawingFirstPixel) {
+        if (!this.bressMode) { // step mode
+            if (this.drawingFirstPixel) {  // if first step
                 this.d = 2 * dy - dx
                 this.x = x0
                 this.y = y0
             }
 
-            if (this.x < x1) {
+            if (this.x < x1) { // if x behind the end point
                 let square = this.getSquareFromCoord(this.x, this.y)
                 this.drawSquare(square)
-                if (this.yi > 0)
+                if (this.yi > 0) // don't draw dots for negative slopes
                     this.drawDots(square)
                 updateXYD(this.x, this.y, this.d)
 
-                if (this.d > 0) {
+                if (this.d > 0) { // see bress algorithm documentation
                     this.y += this.yi
                     this.d -= (2 * dx)
                 }
                 this.d += (2 * dy)
             }
-            this.x++
+            this.x++ // onto the next x
             return
         }
 
-        for (var x = x0; x < x1; x++) {
+        for (var x = x0; x < x1; x++) { // draw line all at once
             let square = this.getSquareFromCoord(x, y)
             this.drawSquare(square)
 
+            // see bress algorithm documentation
             if (d > 0) {
                 y += this.yi
                 d -= (2 * dx)
@@ -185,6 +209,8 @@ function Grid(size, canvas) {
         }
     }
 
+    // draw a line with a steeper slope
+    // same concepts from the function above except we constantly increment y
     this.drawLineHigh = function(x0, y0, x1, y1) {
         let dx = x1 - x0
         let dy = y1 - y0
@@ -197,7 +223,7 @@ function Grid(size, canvas) {
             dx *= -1
         }
 
-        let d = 2 * dx - dy
+        let d = 2 * dx - dy // different
         let x = x0
 
         if (!this.bressMode) {
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// output divs
 let point1El = document.getElementById('point1')
 let point2El = document.getElementById('point2')
 let dxEl = document.getElementById('dx')
@@ -6,24 +7,28 @@ let xEl = document.getElementById('x')
 let yEl = document.getElementById('y')
 let dEl = document.getElementById('d')
 
+// update the following algorithm variables
 function updateXYD(x, y, d) {
     xEl.innerHTML = x
     yEl.innerHTML = y
     dEl.innerHTML = d
 }
 
+// canvas obj
 function Canvas(pWidth, pHeight, pId) {
-    this.id = pId
+    this.id = pId // id of a parent element
     this.tag = document.createElement("canvas")
     this.ctx = this.tag.getContext("2d")
     this.width = pWidth
     this.height = pHeight
 
+    // set css width/height
     this.tag.width = this.width
     this.tag.height = this.height
 
     this.ctx.lineWidth = 1
 
+    // place canvas in the parent element if no parent is given, appendC to body
     let parTag = document.getElementById(this.id)
     if (parTag)
         parTag.appendChild(this.tag)
@@ -31,42 +36,45 @@ function Canvas(pWidth, pHeight, pId) {
         document.body.appendChild(this.tag)
 }
 
+// create grid
 const canvas = new Canvas(500, 500, 'canvas')
 let grid = new Grid(50, canvas)
 grid.draw()
 
+// set grid pixel size
 function setGrid() {
     let dim = document.getElementById('grid-dim').value
     grid = new Grid(dim, canvas)
     grid.draw()
 }
 
-function inSquare(square, x, y) {
-    if (x > square.x && x < (square.x + square.size) &&
-        y > square.y && y < (square.y + square.size))
-        return true
-    return false
-}
-
+// update the coordinate info
 function updatePoints() {
-    if (grid.selected == 1) {
+    if (grid.selected == 1) { // if first point
         let coord = grid.getCoord(grid.squareA)
         let info = "(" + coord[0] + ", " + coord[1] + ")"
         point1El.innerHTML = info
     }
 
-    if  (grid.selected == 2) {
+    if  (grid.selected == 2) { // if second
         let coord = grid.getCoord(grid.squareB)
         let info = "(" + coord[0] + ", " + coord[1] + ")"
         point2El.innerHTML = info
     }
 }
 
+// when the user clicks on the canvas
 canvas.tag.addEventListener('mousedown', (e) => {
+    // do nothing after 2 squares are selected
+    if (grid.selected > 2)
+        return
+
+    // convert from window to local coordinates
     let canvasRect = canvas.tag.getBoundingClientRect()
     let mouseX = e.x - canvasRect.left
     let mouseY = e.y - canvasRect.top
 
+    // fill in the selected square
     let square = grid.getSquare(mouseX, mouseY)
     grid.drawSquare(square)
     updatePoints()
